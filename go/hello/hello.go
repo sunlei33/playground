51ab7ec6af16b72a84cd73e99414fcfb6772b38a
sunlei33 <sunlei33@outlook.com>
2024-01-04 11:26:39 +0800

    feat:新增使用模块

---
+++ go/hello/hello.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+  "fmt"
+  "log"
+  "greetings"
+)
+
+func main(){
+  log.SetPrefix("greetings: ")
+  log.SetFlags(0)
+
+  names := []string{"Gladys", "Samantha", "Darrin", "Lei"}
+  message,err := greetings.Hellos(names)
+  if err != nil{
+    log.Fatal(err)
+  }
+  fmt.Println(message)
+}
